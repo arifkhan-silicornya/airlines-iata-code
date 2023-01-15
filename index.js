841055d0c11e4918be03143725dcb2df6bfebe71
Arif khan <arifkhanshanto123@gmail.com>
2023-01-15 17:07:25 +0600

    airline data update.

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-let airlinesObject = require("./airlineIATA.json");
+let airlinesObject = require("./airline_ver_3.json");
 
 function airline(params) {
     let arr = [];
     let s = 0;
     if (params === "undefined" || params === undefined || params === "") {
         airlinesObject.map((dat) => {
-            s = s + 1;
             arr.push(dat);
         })
     } else {
-        let param = params.toLowerCase()
+        let param = params.toLowerCase();
         airlinesObject.map((dat) => {
             if (dat.IATACode.toLowerCase().search(param) !== -1) {
                 arr.push(dat);
